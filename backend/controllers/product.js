a64085072d042ecd7492cba3a836d0ed731213d4
rommel dalisay <rommelmdalisay@gmail.com>
2026-08-12 11:05:32 +0800

    product crud

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -54,3 +54,115 @@ exports.newProduct = async (req, res, next) => {
         product
     })
 }
+
+exports.getSingleProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+    return res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+exports.getProducts = async (req, res) => {
+
+    // const resPerPage = 4;
+    // const productsCount = await Product.countDocuments();
+
+    const products = await Product.find()
+    // const apiFeatures = new APIFeatures(Product.find(), req.query).search().filter()
+
+    // apiFeatures.pagination(resPerPage);
+    // const products = await apiFeatures.query;
+    // let filteredProductsCount = products.length;
+
+    if (!products)
+        return res.status(400).json({ message: 'error loading products' })
+    return res.status(200).json({
+        success: true,
+        products,
+        // filteredProductsCount,
+        // resPerPage,
+        // productsCount,
+
+    })
+    // if (!products)
+    //     return res.status(400).json({ message: 'error loading products' })
+
+    // return res.status(200).json({
+    //     success: true,
+    //     products,
+    //     resPerPage,
+    //     productsCount,
+
+    // })
+}
+
+exports.updateProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+    // console.log(req.body)
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+    let images = []
+
+    if (typeof req.files === 'string') {
+        images.push(req.files)
+    } else {
+        images = req.files
+    }
+    // if (typeof req.body.images === 'string') {
+    //     images.push(req.body.images)
+    // } else {
+    //     images = req.body.images
+    // }
+    console.log(images)
+
+    let imagesLinks = [];
+    for (let i = 0; i < images.length; i++) {
+        const result = await cloudinary.v2.uploader.upload(images[i].path, {
+            folder: 'products',
+            width: 150,
+            crop: "scale",
+        });
+        imagesLinks.push({
+            public_id: result.public_id,
+            url: result.secure_url
+        })
+
+    }
+    req.body.images = imagesLinks
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindandModify: false
+    })
+    // console.log(product)
+    return res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+exports.deleteProduct = async (req, res, next) => {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+    return res.status(200).json({
+        success: true,
+        message: 'Product deleted'
+    })
+}
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -4,8 +4,16 @@ const upload = require("../utils/multer");
 
 const {
     newProduct,
+    getSingleProduct,
+    getProducts,
+    updateProduct,
+    deleteProduct,
+
 } = require('../controllers/product');
 
 router.post('/admin/product/new', upload.array('images', 10), newProduct);
-
+router.get('/product/:id', getSingleProduct)
+router.get('/products', getProducts)
+router.put('/admin/product/:id', upload.array('images', 10), updateProduct);
+router.delete('/admin/product/:id', deleteProduct);
 module.exports = router
