f6ee8bb395cdc3516c4b226c974434706841fe43
rommel dalisay <rommelmdalisay@gmail.com>
2026-08-12 11:42:21 +0800

    state

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,25 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Product from './Product/Product'
-import products from '../products.json'
+// import products from '../products.json'
 import MetaData from './Layout/MetaData'
+import axios from 'axios'
 const Home = () => {
-    console.log(products)
+    // console.log(products)
+    const [products, setProducts] = useState([])
+
+    const getProducts = async () => {
+
+        let link = `http://localhost:4001/api/v1/products`
+
+        let res = await axios.get(link)
+        console.log(res.data.products)
+        setProducts(res.data.products)
+
+        // setLoading(false)
+    }
+    // getProducts()
+    useEffect(() => {
+        getProducts()
+    }, []);
+
     return (
         <>
             <MetaData title={'shop Here'} />
+
             <div className="container container-fluid">
                 <h1 id="products_heading">Latest Products</h1>
                 <section id="products" className="container mt-5">
                     <div className="row">
                         {products && products.map(product => (
                             // <Product key={product._id} product={product} />
-                            <Product item={product} />
+                            <Product product={product} />
 
 
                         ))}
                     </div>
                 </section>
             </div>
+
+
 
 
         </>
--- frontend/src/Components/Product/Product.jsx
+++ frontend/src/Components/Product/Product.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 
-const Product = ({ item }) => {
-    console.log(item)
+const Product = ({ product }) => {
+    console.log(product)
     return (
         // <ProductDetails item={item} />
         <div className="col-sm-12 col-md-6 col-lg-3 my-3">
             <div className="card p-3 rounded">
                 <img
                     className="card-img-top mx-auto"
-                    src={item.images[0] ? item.images[0].url : null}
+                    src={product.images[0] ? product.images[0].url : null}
                 />
                 <div className="card-body d-flex flex-column">
                     <h5 className="card-title">
-                        <a href="">{item.name}</a>
+                        <a href="">{product.name}</a>
                     </h5>
                     <div className="ratings mt-auto">
                         <div className="rating-outer">
-                            <div className="rating-inner" style={{ width: `${(item.ratings / 5) * 100}%` }}></div>
+                            <div className="rating-inner" style={{ width: `${(product.ratings / 5) * 100}%` }}></div>
                         </div>
-                        <span id="no_of_reviews">({item.numOfReviews} reviews)</span>
+                        <span id="no_of_reviews">({product.numOfReviews} reviews)</span>
                     </div>
-                    <p className="card-text">${item.price}</p>
+                    <p className="card-text">${product.price}</p>
                     {/* <Link to={`/product/${product._id}`} id="view_btn" className="btn btn-block">View Details</Link> */}
                 </div>
             </div>
